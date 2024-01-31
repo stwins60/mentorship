44f374f72c2c2086e0e74a52d535c76d86ce1ed3
Idris Fagbemi <idrisniyi94@gmail.com>
2024-01-30 21:25:39 -0600

    Updated ci

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
+import random
+import secrets
 
+SECRET = secrets.token_hex(64)
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/random-secret')
+def random_secret():
+    return random.choice(SECRET)
 
 
 if __name__ == '__main__':
